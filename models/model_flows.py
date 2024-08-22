ade25bfeb650627bef80282ea0b9e4c97c869146
Reza-Mor <67605247+Reza-Mor@users.noreply.github.com>
2024-08-22 19:05:58 -0400

    Update model_flows.py

--- models/model_flows.py
+++ models/model_flows.py
@@ -5,35 +5,12 @@ import numpy as np
 from torch_geometric.nn import NNConv, GCNConv, GATConv, CGConv, GCN
 from torch_geometric.nn.pool import global_mean_pool 
 from torch_geometric.utils import dense_to_sparse, from_networkx
-from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
-from ray.rllib.utils.framework import try_import_torch
-from ray.rllib.utils.torch_utils import FLOAT_MIN
-torch, nn = try_import_torch()
+import torch
+import torch.nn as nn
 from gymnasium.spaces import Dict
-from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
 from torch_geometric.data import Data, Batch
-
-
-"""
-Processes the input data to obtain a list of flow paths
-Flow_list: Batch x 
-Example: converts [1, 2, 7, 1, 4, 5, 3] with indices [3] to [[1,2,7], [1, 4, 5, 3]].
-[[1, 2, 7], [1, 4, 5, 3]] means flow 0 goes through nodes 1,2,3 and flow 1 goes through nodes 1, 4, 5, 3
-TO DO: this is slow, replace with something faster
-"""
-def get_flow_paths_as_list(flow_lst, indices):
-    print('flow_list: ', flow_lst)
-    print('flow_list: ', flow_lst.shape)
-    print('indices: ', indices)
-    print('indices: ', indices.shape)
-    lst = []
-    # iterate over batch, each instance in the batch is a graph with different reconfigs
-    for i in range(flow_lst.shape[0]):
-        flow_paths = np.split(np.array(flow_lst[i]), indices[i])
-        lst.append(list(map(list, flow_paths)))
-    # change data type
-    return lst
-    
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from torch_geometric.nn import MLP
 """
 node_features: N x node_dim matrix (N being the number of nodes in the graph)
 flows: FG x max_flow_length matrix (FG being the number of flows in the super graph)
@@ -41,13 +18,6 @@ Return a 3D |graphs in the batch| x |F| x |node_dim)| matrix (F = FG/|graphs in 
 Each flow embedding is the sum of the node embeddings
 """
 def get_flow_embeddings(node_features, flow_paths, num_graphs):
-    #flow_embeddings = torch.empty(num_graphs, flow_paths.shape[0] * node_features.shape[1])
-    #F.pad(node_features, (0, 0, 1, 0, 0, 0), mode = 'constant', value = 0)
-        #for i in range(num_graphs):
-    #    flow_embeddings[i] = torch.flatten(
-    #        torch.sum(node_features[i][flow_paths[i]], axis=1)
-    #    )
-
     node_features = F.pad(node_features, (0, 0, 1, 0), mode = 'constant', value = 0)
     # get a |(FG x node_dim)| matrix 
     flow_embeddings = torch.sum(node_features[flow_paths.long()], axis=1)
@@ -56,49 +26,24 @@ def get_flow_embeddings(node_features, flow_paths, num_graphs):
     flow_embeddings = torch.reshape(flow_embeddings, (num_graphs, num_flows_per_graph, node_features.shape[1]))
     return flow_embeddings
 
-
-"""
-node_features: Batcn_size x N x node_dim. torch tensor (N being the number of nodes)
-flows: F x max_flow_length matrix. torch tensor (F being the number of flows)
-Return a 3D Batcn_size x F x node_dim matrix
-Each flow embedding is the sum of the node embeddings
-"""
-def get_flow_embeddings_inv(node_features, flow_paths):
-    
-    flow_embeddings = torch.sum(node_features[flow_paths], axis=1)
-    return torch.flatten(flow_embeddings)
-
-"""
-node_features: Batch x N x node_dim matrix (N being the number of nodes)
-flow_lst: Batch x 
-Return a 2D Batch x (F x node_dim) vector (F being the number of flows)
-Each flow embedding is the sum of the node embeddings
-"""
-def get_flow_embeddings2(node_features, flow_lst, indices):
-    batch_flow_embeddings = torch.empty(node_features.shape[0], indices[1] * node_features.shape[2])
-    # iterate over batch, each instance in the batch is a graph with different reconfigs
-    for i in range(flow_lst.shape[0]):
-        flow_paths = np.split(np.array(flow_lst[i]), indices[i])
-        flow_paths = list(map(list, flow_paths))
-        graph_flow_embeddings = torch.sum(node_features[flow_paths], axis=1)
-
 def convert_to_pyG_batch(input_state):
+    #print("input_state['node_features'].shape: ", input_state['node_features'].shape)
     num_graphs = input_state['node_features'].shape[0]
-    print("input_state['init_config'].shape: ", input_state['init_config'].shape)
+    #print("input_state['init_config'].shape: ", input_state['init_config'].shape)
     data_list = []
 
     #NOTE: check that flows which do not appear in the graph are represented as a vector of shape [1,longest_flow_length] containing only zeros
     num_prev_nodes = 0
     for i in range(num_graphs):
-        non_zero_indices_init = (input_state['init_config'][i] != 0).int()
-        non_zero_indices_target = (input_state['target_config'][i] != 0).int()
+        node_indices_init = (input_state['init_config'][i] != -1).int()
+        node_indices_target = (input_state['target_config'][i] != -1).int()
         data_list.append(
             Data(x=input_state['node_features'][i],
                  edge_index= input_state['edge_indices'].type(torch.int64)[i], 
                  edge_features= input_state['edge_features'][i],
-                 selected_flows = input_state['selected_flows'][i],
-                 init_config = input_state['init_config'][i] + num_prev_nodes * non_zero_indices_init ,
-                 target_config = input_state['target_config'][i] + num_prev_nodes * non_zero_indices_target,
+                 selected_flows = torch.unsqueeze(input_state['selected_flows'][i], 0) ,
+                 init_config = input_state['init_config'][i] + num_prev_nodes * node_indices_init ,
+                 target_config = input_state['target_config'][i] + num_prev_nodes * node_indices_target,
                  flow_sizes = input_state['flow_sizes'][i]
                  )
             )
@@ -107,12 +52,90 @@ def convert_to_pyG_batch(input_state):
     return batch
 
 
-#class GCN(torch.nn.Module):
+class GCN_Flows(BaseFeaturesExtractor):
+    """
+    :param observation_space: (gym.Space)
+    :param features_dim: (int) Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    """
+
+    def __init__(self, observation_space, cfg, features_dim=1):
+        super().__init__(observation_space, features_dim=1)
+        torch.manual_seed(cfg.seed)
+        
+        self.node_feature_dim = 3
+        self.edge_features = 3
+        self.out_channels = cfg.out_channels
+        self.num_flows = cfg.num_flows #same as number of actions
+        #self.num_nodes = 30
+        self.graph_dim = cfg.graph_dim
+        self._features_dim = self.out_channels + self.num_flows
+
+        # Node level linear layer
+        self.nn1 = MLP(in_channels=self.node_feature_dim , out_channels=self.graph_dim, num_layers= 1, norm="batchnorm")
+        self.relu = ReLU()
+
+        # GNN Encoder
+        self.conv1 = GATConv(in_channels = self.graph_dim, hidden_channels= self.graph_dim, out_channels = self.graph_dim, edge_dim = 3, num_layers = 2, norm="batchnorm")
+        
+        # GNN Graph Level Decoder
+        self.nn2 = Sequential(
+            Linear(self.graph_dim, self.out_channels),
+            ReLU(),
+            #Linear(self.out_channels, self.out_channels),
+        )
+        
+
+    def forward(self, observations):
+        # Data to be passed to the GCN. 
+        # Node features are initialized randomly
+        # Edge features are the total edge capacity, current free edge capacity , current used edge capacity 
+        
+        data = convert_to_pyG_batch(observations)
+        
+        # Pass the node features to simple linear layer
+        x = self.nn1(data.x)
+        x = self.relu(x)
+
+        # pass the features to the GNN
+        x = self.conv1(x, data.edge_index, data.edge_features)
+
+        # get embeddings for the flows
+        init_flow_emebeddings = get_flow_embeddings(x, data.init_config, data.num_graphs)
+        target_flow_embeddings = get_flow_embeddings(x, data.target_config, data.num_graphs)
+
+        graph_embedding = global_mean_pool(x, data.batch)
+        graph_embedding = graph_embedding.unsqueeze(1)
+
+        # get a BS x (2*|number of flows| + 1) x embedding_dim matrix
+        x = torch.cat([graph_embedding, init_flow_emebeddings, target_flow_embeddings], axis = 1)
+        # pass to the decoder
+        x = self.nn2(x)
+        
+        # obtain a final embedding by taking the mean of graph and flow embedding - TO DO: replace with attention
+        x = x.mean(1)
+        # include information about which flows are selected
+        x = torch.cat([x, data.selected_flows], axis = 1)
+
+        return x
+
+        """
+        self._value = self._critic_head(x)
+        logits = self._actor_head(x)
+        #if data.num_graphs == 1:
+            #self._value = self._value.reshape(-1)
+            #logits = logits.reshape(-1)
+        #print('value: ', self._value.shape)
+        #print('logits: ', logits.shape)
+        return logits, state
+        """
+
+"""
 class GCN_Flows(TorchModelV2, nn.Module):
-    def __init__(self, obs_space, action_space, num_outputs, model_config, name, **kwargs):
+    def __init__(self, obs_space, action_space, lr_schedule, *args, **kwargs):
         
         TorchModelV2.__init__(
-            self, obs_space, action_space, num_outputs, model_config, name, **kwargs
+            self, obs_space, action_space, lr_schedule, model_config, name, **kwargs
         )
         nn.Module.__init__(self)
         self.var_list = [] # for rllib use
@@ -154,10 +177,10 @@ class GCN_Flows(TorchModelV2, nn.Module):
         # Node features are initialized randomly
         # Edge features are the total edge capacity, current free edge capacity , current used edge capacity 
         
-
+        #print('rllib data: ', input_dict["obs"])
         data = convert_to_pyG_batch(input_dict["obs"])
 
-        print(data)
+        print('pyG data: ', data)
         #print(input_dict)
         #print(input_state)
 
@@ -172,10 +195,10 @@ class GCN_Flows(TorchModelV2, nn.Module):
 
         # get a BS x (2*|number of flows| + 1) x embedding_dim matrix
         x = torch.cat([graph_embedding, init_flow_emebeddings, target_flow_embeddings], axis = 1)
-        print('x: ', x.shape)
+        #print('x: ', x.shape)
         #x = torch.cat((torch.unsqueeze(action_mask, 2),  torch.unsqueeze(selected_nodes, 2)), 2)
         x = self.nn1(x)
-        print('x1: ', x.shape)
+        #print('x1: ', x.shape)
         # obtain a final embedding by taking the mean of graph and flow embedding - TO DO: replace with attention
         x = x.mean(1)
         self._value = self._critic_head(x)
@@ -183,8 +206,8 @@ class GCN_Flows(TorchModelV2, nn.Module):
         #if data.num_graphs == 1:
             #self._value = self._value.reshape(-1)
             #logits = logits.reshape(-1)
-        print('value: ', self._value.shape)
-        print('logits: ', logits.shape)
+        #print('value: ', self._value.shape)
+        #print('logits: ', logits.shape)
         return logits, state
 
         #self.values = F.softmax(x, dim=1)
@@ -204,5 +227,5 @@ class GCN_Flows(TorchModelV2, nn.Module):
 
     def set_adj_matrix_sparse(self, adj_matrix):
         self.adj_matrix = dense_to_sparse(from_networkx(adj_matrix))
-        
+"""  
 
